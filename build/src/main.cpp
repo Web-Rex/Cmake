903af53d7b2f9d37ef35c1d1ba74ddceaa2c2bf6
Christopher Victor <victor79646@gmail.com>
2023-01-19 02:36:08 -0800

    new

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -3,10 +3,22 @@
 
 #include "main.h"
 
+#ifndef CMAKEMACRO1
+#define CMAKEMACRO1 "NO SYSTEM NAME"
+#endif
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "Hello CMake." << std::endl;
+	std::cout << CMAKEMACRO1 << std::endl;
+
+	#ifdef AWESOME
+		std::cout << "YOU ARE AWESOME" << std::endl;
+	#else
+		std::cout << "Sorry, you are not awesome" << std::endl;
+	#endif
+
 	return 0;
 }
